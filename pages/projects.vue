bd123bb523ce8b51fe189aaa620854df16c51a56
Justin Forlenza <justin@justinforle.nz>
2020-06-06 16:13:17 -0400

    Started Work On Projects Page
    - Addded fetch method
    - Added CSS to properly align page

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,11 +2,32 @@
   <div>
     <h1>Projects</h1>
     <nuxt-child />
+    <div>
+      <h1>Projects</h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    projects: []
+  }),
+  async fetch () {
+    this.projects = await this.$http.$get('/api/projects')
+  }
 }
 </script>
+
+<style lang="postcss" scoped>
+main > div {
+  @apply mx-8;
+  @apply pt-16;
+  @screen lg {
+    @apply mx-16;
+  }
+  @screen xl {
+    @apply mx-64
+  }
+}
+</style>
